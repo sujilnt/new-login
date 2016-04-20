e8cd792a02f4e1a2b59013c88cce08953ee796a5
Sujil Anto <sujilnt@gmail.com>
2016-04-21 02:21:43 +0530

    updated login page

---
+++ images/visualCaptcha - The best captcha alternative_files/main.js
@@ -0,0 +1,20 @@
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+//A lightweight, easy-to-use jQuery plugin for fluid width video embeds. 
+$(document).ready(function(){
+    // Target your .container, .wrapper, .post, etc.
+    $(".video").fitVids();
+});
